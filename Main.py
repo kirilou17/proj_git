4bad1bf3f75d2175bbbe4aab8cc26c4d211273f3
Kirill <kapokapokino@gmail.com>
2025-04-23 21:39:14 +0300

    Basic calculator

--- Main.py
+++ Main.py
@@ -1,2 +1,17 @@
-print("hello")
-print(122)
+def operator_sel():
+    num_1 = int(input("Enter first number: "))
+    num_2 = int(input("Enter second number: "))
+
+    operator_func = str(input("Enter operator(+, -, *, /): "))
+
+    operations = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y if y != 0 else 'Division by zero error'
+    }
+
+    result = operations.get(operator_func, lambda x, y: 'Invalid operator')(num_1, num_2)
+    print("Result:", result)
+        
+operator_sel()
