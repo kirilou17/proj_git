5fcec93be36a78698b0d9b756a339615d50e6536
Kirill <kapokapokino@gmail.com>
2025-04-23 21:43:43 +0300

    Add pow(**)

--- Main.py
+++ Main.py
@@ -1,14 +1,18 @@
+import math
+
 def operator_sel():
     num_1 = int(input("Enter first number: "))
     num_2 = int(input("Enter second number: "))
 
-    operator_func = str(input("Enter operator(+, -, *, /): "))
+    operator_func = str(input("Enter operator(+, -, **, /): "))
 
     operations = {
         '+': lambda x, y: x + y,
         '-': lambda x, y: x - y,
         '*': lambda x, y: x * y,
+        '**': lambda x, y: pow(x, y),
         '/': lambda x, y: x / y if y != 0 else 'Division by zero error'
+        
     }
 
     result = operations.get(operator_func, lambda x, y: 'Invalid operator')(num_1, num_2)
