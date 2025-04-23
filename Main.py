fb6d63420f8f9b4aa15af8fa6a7d67c4e49c0fed
Kirill <kapokapokino@gmail.com>
2025-04-23 21:47:21 +0300

    Add factorial

--- Main.py
+++ Main.py
@@ -1,16 +1,17 @@
-import math
+from math import *
 
 def operator_sel():
     num_1 = int(input("Enter first number: "))
     num_2 = int(input("Enter second number: "))
 
-    operator_func = str(input("Enter operator(+, -, **, /): "))
+    operator_func = str(input("Enter operator(+, -, **, /, !): "))
 
     operations = {
         '+': lambda x, y: x + y,
         '-': lambda x, y: x - y,
         '*': lambda x, y: x * y,
         '**': lambda x, y: pow(x, y),
+        '!': lambda x, y: factorial(x) + factorial(y),
         '/': lambda x, y: x / y if y != 0 else 'Division by zero error'
         
     }
