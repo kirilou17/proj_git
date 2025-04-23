8c411055736ca7aa43a7b6c80c1fab47793d7a84
Kirill <kapokapokino@gmail.com>
2025-04-23 21:51:25 +0300

    Add %

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ def operator_sel():
     num_1 = int(input("Enter first number: "))
     num_2 = int(input("Enter second number: "))
 
-    operator_func = str(input("Enter operator(+, -, **, /, !): "))
+    operator_func = str(input("Enter operator(+, -, **, /, !, %(на скікльки процентів х < y)): "))
 
     operations = {
         '+': lambda x, y: x + y,
@@ -12,6 +12,7 @@ def operator_sel():
         '*': lambda x, y: x * y,
         '**': lambda x, y: pow(x, y),
         '!': lambda x, y: factorial(x) + factorial(y),
+        '%': lambda x, y: ((x - y) / y) * 100,
         '/': lambda x, y: x / y if y != 0 else 'Division by zero error'
         
     }
